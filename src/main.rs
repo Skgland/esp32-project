a90df085d7b2928a27100e8784a1af566b37ff2c
Bennet Bleßmann <bb-github@t-online.de>
2023-06-07 21:52:52 +0200

    cycle between two images

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{error::Error};
+use std::{error::Error, time::Duration};
 
 use embedded_graphics_core::pixelcolor::{Rgb565, Rgb888, raw::ToBytes};
 use display_interface_spi::SPIInterfaceNoCS;
-use mipidsi::Builder; 
+use mipidsi::Builder;
 use embedded_graphics::{prelude::*, image::{ImageRawBE, ImageRaw}};
 use esp_idf_hal::{
     delay::Ets,
@@ -23,7 +23,7 @@ fn main()  {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    
+
     println!("Hello, world!");
 
     let rst = PinDriver::input_output_od(unsafe { Gpio23::new() })?;
@@ -33,7 +33,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     let sclk = unsafe { Gpio18::new() };
 	let spi = unsafe { SPI2::new() };
     let sdo = unsafe { Gpio19::new() };
-    
+
     let spi = SpiDriver::new(
         spi,
         sclk,
@@ -46,40 +46,67 @@ fn run() -> Result<(), Box<dyn Error>> {
     let cs = unsafe { Gpio5::new() };
 
     let spi = SpiDeviceDriver::new(spi, Some(cs), &Config::new())?;
-    
+
     let di = SPIInterfaceNoCS::new(spi, dc);
     let mut display = Builder::st7789_pico1(di)
         .init(&mut delay, Some(rst))
 	    .map_err(|err| Box::<dyn Error>::from(format!("{err:?}")))?;
-    
+
     display.clear(Rgb565::RED)
         .map_err(|err| Box::<dyn Error>::from(format!("{err:?}")))?;
 
     let mut bl = PinDriver::input_output_od(unsafe { Gpio4::new() })?;
     bl.set_high()?;
 
-    
-    let img_data = include_bytes!("qoi_logo-240x135.qoi");
-    static mut IMG : [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
 
-    let Some((header , pixels)) = arqoii::QoiDecoder::new(img_data.iter().copied()) else {
+    let qoi_logo_data = include_bytes!("qoi_logo-240x135.qoi");
+    let cube4_data = include_bytes!("Qube4-esp32.qoi");
+    static mut QOI_LOGO : [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
+    static mut CUBE_4 : [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
+
+
+    println!("Decoding Qoi Image Data");
+
+    let Some((qoi_logo_header , pixels)) = arqoii::QoiDecoder::new(qoi_logo_data.iter().copied()) else {
         return Err(Box::<dyn Error>::from("Qoi Decoding Error"))
     };
-    
-    println!("Decoding Qoi Image Data");
-    
-    for (dest_pixel, src_pixel) in unsafe {IMG.chunks_exact_mut(2)}.zip(pixels) {
+
+    for (dest_pixel, src_pixel) in unsafe {QOI_LOGO.chunks_exact_mut(2)}.zip(pixels) {
         let be_pixel = Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
         dest_pixel[0] = be_pixel[0];
         dest_pixel[1] = be_pixel[1];
     }
 
-    println!("Displaying Image");
-    
-    let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe {&IMG}, header.width);
-        raw
-        .draw(&mut display)
-        .map_err(|_| Box::<dyn Error>::from("draw image"))?;
+    let Some((cube4_header , pixels)) = arqoii::QoiDecoder::new(cube4_data.iter().copied()) else {
+        return Err(Box::<dyn Error>::from("Qoi Decoding Error"))
+    };
 
+    for (dest_pixel, src_pixel) in unsafe {CUBE_4.chunks_exact_mut(2)}.zip(pixels) {
+        let be_pixel = Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
+        dest_pixel[0] = be_pixel[0];
+        dest_pixel[1] = be_pixel[1];
+    }
+
+    loop {
+        println!("Displaying: Qoi Logo");
+
+        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe {&QOI_LOGO}, qoi_logo_header.width);
+            raw
+            .draw(&mut display)
+            .map_err(|_| Box::<dyn Error>::from("draw image"))?;
+
+        std::thread::sleep(Duration::from_secs(5));
+
+        println!("Displaying: Cube 4");
+
+        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe {&CUBE_4}, cube4_header.width);
+            raw
+            .draw(&mut display)
+            .map_err(|_| Box::<dyn Error>::from("draw image"))?;
+
+        std::thread::sleep(Duration::from_secs(5));
+    }
+
+    #[allow(unreachable_code)]
     Ok(())
 }
