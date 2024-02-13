6e22ea826920deb5a6b5f5ea8deadb09d7cc7ec5
Skgland <bennet.blessmann+github@googlemail.com>
2024-02-14 00:21:46 +0100

    update/upgrade dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::{error::Error, time::Duration};
 
-use embedded_graphics_core::pixelcolor::{Rgb565, Rgb888, raw::ToBytes};
 use display_interface_spi::SPIInterfaceNoCS;
-use mipidsi::Builder;
-use embedded_graphics::{prelude::*, image::{ImageRawBE, ImageRaw}};
+use embedded_graphics::{
+    image::{ImageRaw, ImageRawBE},
+    prelude::*,
+};
+use embedded_graphics_core::pixelcolor::{raw::ToBytes, Rgb565, Rgb888};
 use esp_idf_hal::{
     delay::Ets,
-    gpio::{Gpio16, Gpio18, Gpio19, Gpio23, Gpio5, PinDriver, AnyIOPin, Gpio4},
-    spi::{config::Config, Dma, SpiDeviceDriver, SpiDriver, SPI2},
+    gpio::{AnyIOPin, Gpio16, Gpio18, Gpio19, Gpio23, Gpio4, Gpio5, PinDriver},
+    spi::{
+        config::{Config, DriverConfig},
+        Dma, SpiDeviceDriver, SpiDriver, SPI2,
+    },
 };
+use mipidsi::Builder;
 
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-fn main()  {
+fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
@@ -23,7 +29,6 @@ fn main()  {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-
     println!("Hello, world!");
 
     let rst = PinDriver::input_output_od(unsafe { Gpio23::new() })?;
@@ -31,7 +36,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     let mut delay = Ets;
 
     let sclk = unsafe { Gpio18::new() };
-	let spi = unsafe { SPI2::new() };
+    let spi = unsafe { SPI2::new() };
     let sdo = unsafe { Gpio19::new() };
 
     let spi = SpiDriver::new(
@@ -39,9 +44,11 @@ fn run() -> Result<(), Box<dyn Error>> {
         sclk,
         sdo,
         None::<AnyIOPin>,
-        Dma::Channel1(240 * 135 * 2 + 8), // 2 bytes per pixel: 5 bit red, 6 bit green, 5 bit blue
+        &DriverConfig::new().dma(
+            // Dma::Channel1(240 * 135 * 2 + 8), // 2 bytes per pixel: 5 bit red, 6 bit green, 5 bit blue
+            Dma::Channel1(0x1000), // must be multiple of 4 and in 1..=4096 i.e not 0 and <= 0x1000
+        ),
     )?;
-
 
     let cs = unsafe { Gpio5::new() };
 
@@ -50,39 +57,41 @@ fn run() -> Result<(), Box<dyn Error>> {
     let di = SPIInterfaceNoCS::new(spi, dc);
     let mut display = Builder::st7789_pico1(di)
         .init(&mut delay, Some(rst))
-	    .map_err(|err| Box::<dyn Error>::from(format!("{err:?}")))?;
+        .map_err(|err| Box::<dyn Error>::from(format!("{err:?}")))?;
 
-    display.clear(Rgb565::RED)
+    display
+        .clear(Rgb565::RED)
         .map_err(|err| Box::<dyn Error>::from(format!("{err:?}")))?;
 
     let mut bl = PinDriver::input_output_od(unsafe { Gpio4::new() })?;
     bl.set_high()?;
 
-
     let qoi_logo_data = include_bytes!("qoi_logo-240x135.qoi");
     let cube4_data = include_bytes!("Qube4-esp32.qoi");
-    static mut QOI_LOGO : [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
-    static mut CUBE_4 : [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
-
+    static mut QOI_LOGO: [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
+    static mut CUBE_4: [u8; 240 * 135 * 2] = [0u8; 240 * 135 * 2];
 
     println!("Decoding Qoi Image Data");
 
-    let Some((qoi_logo_header , pixels)) = arqoii::QoiDecoder::new(qoi_logo_data.iter().copied()) else {
-        return Err(Box::<dyn Error>::from("Qoi Decoding Error"))
+    let Some((qoi_logo_header, pixels)) = arqoii::QoiDecoder::new(qoi_logo_data.iter().copied())
+    else {
+        return Err(Box::<dyn Error>::from("Qoi Decoding Error"));
     };
 
-    for (dest_pixel, src_pixel) in unsafe {QOI_LOGO.chunks_exact_mut(2)}.zip(pixels) {
-        let be_pixel = Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
+    for (dest_pixel, src_pixel) in unsafe { QOI_LOGO.chunks_exact_mut(2) }.zip(pixels) {
+        let be_pixel =
+            Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
         dest_pixel[0] = be_pixel[0];
         dest_pixel[1] = be_pixel[1];
     }
 
-    let Some((cube4_header , pixels)) = arqoii::QoiDecoder::new(cube4_data.iter().copied()) else {
-        return Err(Box::<dyn Error>::from("Qoi Decoding Error"))
+    let Some((cube4_header, pixels)) = arqoii::QoiDecoder::new(cube4_data.iter().copied()) else {
+        return Err(Box::<dyn Error>::from("Qoi Decoding Error"));
     };
 
-    for (dest_pixel, src_pixel) in unsafe {CUBE_4.chunks_exact_mut(2)}.zip(pixels) {
-        let be_pixel = Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
+    for (dest_pixel, src_pixel) in unsafe { CUBE_4.chunks_exact_mut(2) }.zip(pixels) {
+        let be_pixel =
+            Rgb565::from(Rgb888::new(src_pixel.r, src_pixel.g, src_pixel.b)).to_be_bytes();
         dest_pixel[0] = be_pixel[0];
         dest_pixel[1] = be_pixel[1];
     }
@@ -90,18 +99,16 @@ fn run() -> Result<(), Box<dyn Error>> {
     loop {
         println!("Displaying: Qoi Logo");
 
-        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe {&QOI_LOGO}, qoi_logo_header.width);
-            raw
-            .draw(&mut display)
+        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe { &QOI_LOGO }, qoi_logo_header.width);
+        raw.draw(&mut display)
             .map_err(|_| Box::<dyn Error>::from("draw image"))?;
 
         std::thread::sleep(Duration::from_secs(5));
 
         println!("Displaying: Cube 4");
 
-        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe {&CUBE_4}, cube4_header.width);
-            raw
-            .draw(&mut display)
+        let raw: ImageRawBE<Rgb565> = ImageRaw::new(unsafe { &CUBE_4 }, cube4_header.width);
+        raw.draw(&mut display)
             .map_err(|_| Box::<dyn Error>::from("draw image"))?;
 
         std::thread::sleep(Duration::from_secs(5));
